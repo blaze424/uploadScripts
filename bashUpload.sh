6bbedf2ebd27a1bfe43c46a813105add0f66bb53
blaze <secndary.123@gmail.com>
2025-02-22 15:46:10 +0530

    undo last commit

--- bashUpload.sh
+++ bashUpload.sh
@@ -4,6 +4,6 @@ rm -rf uploads
 mkdir uploads
 
 mv out/target/product/*/*.zip uploads
-mv out/target/product/sweet/recovery.img uploads
+mv out/target/product/*/recovery.img uploads
 
 tar -czvf /tmp/uploads.tgz uploads && curl https://bashupload.com/uploads.tgz --data-binary @/tmp/uploads.tgz
