896f89a160661568fec86412aae2b60d58dc7400
Ablazry <secndary.123@gmail.com>
2025-02-22 15:26:58 +0530

    Added bashUpload script

---
+++ bashUpload.sh
@@ -0,0 +1,2 @@
+rm -rf uploads && mkdir uploads && mv out/target/product/*/*.zip uploads && mv out/target/product/sweet/recovery.img uploads && tar -czvf /tmp/uploads.tgz uploads 
+&& curl https://bashupload.com/uploads.tgz --data-binary @/tmp/uploads.tgz
