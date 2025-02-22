d87839f430704ccad5a8f31420fea820a13b43c1
Ablazry <secndary.123@gmail.com>
2025-02-22 15:34:11 +0530

    update bashUpload.sh

--- bashUpload.sh
+++ bashUpload.sh
@@ -1,2 +1,9 @@
-rm -rf uploads && mkdir uploads && mv out/target/product/*/*.zip uploads && mv out/target/product/sweet/recovery.img uploads && tar -czvf /tmp/uploads.tgz uploads 
-&& curl https://bashupload.com/uploads.tgz --data-binary @/tmp/uploads.tgz
+#!/bin/bash
+
+rm -rf uploads
+mkdir uploads
+
+mv out/target/product/*/*.zip uploads
+mv out/target/product/*/recovery.img uploads
+
+tar -czvf /tmp/uploads.tgz uploads && curl https://bashupload.com/uploads.tgz --data-binary @/tmp/uploads.tgz
